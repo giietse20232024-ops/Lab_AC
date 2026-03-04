bec91c960354c10a169e960e45a7de017c8455cc
Nestorfuster <Nestorfuster@github.com>
2026-03-04 12:24:35 +0100

    Nada importante

--- Lab_1/Nestor/Ejercicio_1.c
+++ Lab_1/Nestor/Ejercicio_1.c
@@ -4,7 +4,7 @@
 int main() {
 int nthreads, thread;
 
-  #pragma omp parallel private(nthreads, thread)
+  #pragma omp parallel private(nthreads, thread) omp_set_num_theads(2)
   {
     thread = omp_get_thread_num();
     nthreads = omp_get_num_threads();
@@ -13,3 +13,8 @@ int nthreads, thread;
 
 return 0;
 }
+
+// P1: Un camvio simple:
+// P2: al hacer eso unos se sobrescribirian a otros las variables dando resultados no fiables
+// P3: Al pasarle la variable como private, cada uno tendra una copia diferente que no podra modificar ningun otro proceso.
+// P4: Se puede poner un if que identifique si es el thread = 0;
