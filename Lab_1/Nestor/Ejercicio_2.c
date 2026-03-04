23bb5799e47109b28875c2533fd01b245c41a92b
Nestorfuster <Nestorfuster@github.com>
2026-03-04 12:40:27 +0100

    Nada importante

--- Lab_1/Nestor/Ejercicio_2.c
+++ Lab_1/Nestor/Ejercicio_2.c
@@ -10,7 +10,7 @@ int tid, i;
 
 omp_set_num_threads(NTHREADS);
 
-#pragma omp parallel private(tid)
+#pragma omp for schedule(static) parallel private(tid)
 {
   tid = omp_get_thread_num();
 
@@ -23,3 +23,10 @@ omp_set_num_threads(NTHREADS);
 
   return 0;
 }
+
+// P1.1: cada una ejecuta N/NTHREADS iteraciones, en este caso 2
+// P1.2: Las asignaciones son ejecutuvas porque se han hecho de forma statica.
+// P1:3: La carga no esta equilibrada.
+
+// P2.1: La ejecucion se mantiene igual
+// P2.2: L
